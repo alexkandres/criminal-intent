cac63846baaedfaeda2cf1cb2d071fb03131b7c5
Alex Andres <alexkandres@live.com>
2015-05-26 23:29:34 -0400

    Getters And Setter

--- app/src/main/java/com/example/alex/criminalintent/Crime.java
+++ app/src/main/java/com/example/alex/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.alex.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,10 +10,13 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime(){
         //Generates unique id
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId(){
@@ -25,5 +29,21 @@ public class Crime {
 
     public void setTitle(String mTitle){
         this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
